194cc4d8b49e48210cc92e64b1a30083b4bf991f
Jake Murphy <jcmurph1228@gmail.com>
2022-04-19 15:08:18 -0700

    Added CrossOrigin support

--- workoutapp-backend/src/main/java/com/workoutapp/workoutapp/dao/WeightliftingMovementsRepository.java
+++ workoutapp-backend/src/main/java/com/workoutapp/workoutapp/dao/WeightliftingMovementsRepository.java
@@ -4,7 +4,9 @@ import com.workoutapp.workoutapp.entity.WeightliftingMovements;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "weightliftingMovements", path = "weightlifting-movements")
 public interface WeightliftingMovementsRepository extends JpaRepository<WeightliftingMovements, String>{
     
--- workoutapp-backend/src/main/java/com/workoutapp/workoutapp/dao/WorkoutDetails.java
+++ workoutapp-backend/src/main/java/com/workoutapp/workoutapp/dao/WorkoutDetails.java
@@ -2,7 +2,9 @@ package com.workoutapp.workoutapp.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "workoutDetails", path = "workout-details")
 public interface WorkoutDetails extends JpaRepository<WorkoutDetails, Long>{
     
