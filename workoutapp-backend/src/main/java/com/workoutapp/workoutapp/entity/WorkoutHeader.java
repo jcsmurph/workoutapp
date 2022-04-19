eef80a6794fc5ca5f11f109694f13ea191e0d41a
Jake Murphy <jcmurph1228@gmail.com>
2022-04-19 14:49:37 -0700

    Change from Workout to Workout Header to align with DB

---
+++ workoutapp-backend/src/main/java/com/workoutapp/workoutapp/entity/WorkoutHeader.java
@@ -0,0 +1,37 @@
+package com.workoutapp.workoutapp.entity;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "workout_header")
+@Data
+public class WorkoutHeader {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "workout_id")
+    private Long workoutId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Long userId;
+
+        @Column(name = "day_of_week")
+    private String dayOfWeek;
+
+    @Column(name = "date_of_workout")
+    private Date dateOfWorkout;
+
+}
